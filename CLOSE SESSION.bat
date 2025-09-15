0f4ce505bb646417591d5d618629b0906c988585
Fouad Azouzi <128646294+FouadAzouzi@users.noreply.github.com>
2025-09-15 10:42:25 +0200

    Add files via upload

---
+++ CLOSE SESSION.bat
@@ -0,0 +1,3 @@
+@echo off
+powershell -ExecutionPolicy Bypass -File "C:\RDS-FROZEN-SESSIONS.ps1"
+pause
---
+++ RDS-FROZEN-SESSIONS.ps1
@@ -0,0 +1,29 @@
+﻿$usuario = $env:USERNAME
+$servidores = @("172.20.64.30", "172.20.64.31", "172.20.64.33", "172.20.64.34", "172.20.64.35", "172.20.64.36", "172.20.64.37")
+$sesionesActivas = $false
+
+foreach ($servidor in $servidores) {
+    Write-Host "Buscando sesiones..."
+    $sesiones = quser /server:$servidor 2>$null | Where-Object { $_ -match $usuario }
+
+    if ($sesiones) {
+        $sesionesActivas = $true
+        Write-Host "Se encontraron sesiones activas para $usuario."
+
+        $confirmacion = Read-Host "¿Deseas cerrarlas? (S/N)  S= Cerrar ; N= No cerrar"
+        if ($confirmacion -eq "S") {
+            foreach ($sesion in $sesiones) {
+                $idSesion = ($sesion -split "\s+")[2]
+                logoff $idSesion /SERVER:$servidor
+            }
+            Write-Host "Sesiones cerradas exitosamente."
+        } else {
+            Write-Host "No se han cerrado sesiones."
+        }
+    }
+}
+
+if (-not $sesionesActivas) {
+    Write-Host "No se encontraron sesiones activas para $usuario en ninguno de los servidores."
+}
+exit
